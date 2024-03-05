4a05ca05a1bdd5fb2f287a698e75d036a119f655
pasta777 <axegamerssrb@gmail.com>
2024-03-05 08:13:17 +0100

    promenjeno ime prozora

--- src/main.cpp
+++ src/main.cpp
@@ -121,7 +121,7 @@ int main() {
 
     // glfw window creation
     // --------------------
-    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Chess and Lamp", NULL, NULL);
     if (window == NULL) {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
